2988dbbe18e2b112ac4916427a272935d9f096a5
Kim Vo <126284919+KimVVo@users.noreply.github.com>
2024-04-19 09:37:02 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select the "Shop Now" button
+const shopNowBtn = document.querySelector('.shop-now-btn');
+
+// Add a click event listener to the button
+shopNowBtn.addEventListener('click', function() {
+  alert('Welcome to our shop!');
+});
